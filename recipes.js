88d93426cf505e237d91b12885844072b8fdcaa6
Abqu2 <38745336+Abqu2@users.noreply.github.com>
2018-05-02 14:01:11 -0500

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -79,6 +79,66 @@ theBloopinatorKnife.cost = {
 }
 blueprints.push(theBloopinatorKnife)
 
+const clubMace = new item("Club",7,10000);
+clubMace.cost = {
+    "Wood":5,
+}
+blueprints.push(clubMace)
+
+const maulMace = new item("Maul",12,16000);
+maulMace.cost = {
+    "Wood":8,
+}
+blueprints.push(maulMace)
+
+const brawlerMace = new item("Mace",15,19000);
+brawlerMace.cost = {
+    "Wood":11,
+}
+blueprints.push(brawlerMace)
+
+const broomMace = new item("The Broominator",20,22000);
+broomMace.cost = {
+    "Wood":14,
+}
+blueprints.push(broomMace)
+
+const blackjackMace = new item("Blackjack",26,26000);
+blackjackMace.cost = {
+    "Wood":19,
+}
+blueprints.push(blackjackMace)
+
+const bludgeonMace = new item("Bludgeon",24,30000);
+bludgeonMace.cost = {
+    "Wood":24,
+}
+blueprints.push(bludgeonMace)
+
+const stripedClubMace = new item("Striped Club",44,35000);
+stripedClubMace.cost = {
+    "Wood":31,
+}
+blueprints.push(stripedClubMace)
+
+const nightClubMace = new item("Night Club",57,40000);
+nightClubMace.cost = {
+    "Wood":41,
+}
+blueprints.push(nightClubMace)
+
+const factFinderMace = new item("Fact Finder",74,46000);
+factFinderMace.cost = {
+    "Wood":53,
+}
+blueprints.push(factFinderMace)
+
+const knifeClubMace = new item("Knife Club",97,53000);
+knifeClubMace.cost = {
+    "Wood":69,
+}
+blueprints.push(knifeClubMace)
+
 const imageReference = {
     "Coin" : '<img src="PixelItem/CoinsGold5.png">',
     "Ore" : '<img src="PixelItem/Ore.png">',
@@ -92,4 +152,14 @@ const imageReference = {
     "Club Knife" : '<img src="PixelItem/Equip/Set/FineBlade2.png">',
     "Dark Is The Knife" : '<img src="Pixeltiers_16x16_RPG_Pack_V1.35/swords/sword_45.png">',
     "The Bloopinator" : '<img src="PixelItem/Equip/Set/FieryBlade3.png">',
+    "clubMace" : '<img src="PixelItem/Equip/Weapon/Club.png">',
+    "maulMace" : '<img src="Pixeltiers_16x16_RPG_Pack_V1.35/hammers/hammer_12.png">',
+    "brawlerMace" : '<img src="PixelItem/Equip/Weapon/Mace1.png">',
+    "broomMace" : '<img src="Pixeltiers_16x16_RPG_Pack_V1.35/misc/broom_1.png">',
+    "blackjackMace" : '<img src="PixelItem/Equip/Weapon/Mace2.png">',
+    "bludgeonMace" : '<img src="PixelItem/Equip/Weapon/Mace0.png">',
+    "stripedClubMace" : '<img src="Pixeltiers_16x16_RPG_Pack_V1.35/hammers/hammer_9.png">',
+    "nightClubMace" : '<img src="Pixeltiers_16x16_RPG_Pack_V1.35/hammers/hammer_30.png">',
+    "factFinderMace" : '<img src="Pixeltiers_16x16_RPG_Pack_V1.35/hammers/hammer_1.png">',
+    "knifeClubMace" : '<img src="Pixeltiers_16x16_RPG_Pack_V1.35/magic-weapons/magicweapon_1.png">',
 }
