0e2ba47485d5caebdd8f842bc7481f5896d953b5
zestyhamburger <richardpichardo@outlook.com>
2018-05-11 16:47:04 -0400

    further additions to theme support

--- style.js
+++ style.js
@@ -47,8 +47,15 @@ function loadTheme() {
 function lightTheme() {
     root.style.setProperty('--theme-bg', '#fff');
     root.style.setProperty('--theme-bg-2', '#f3f3f3');
+    root.style.setProperty('--theme-bg-3', '#f9f7f7');
     root.style.setProperty('--theme-border', '#d1d1d1');
+    root.style.setProperty('--theme-border-2', '#adadad');
     root.style.setProperty('--theme-text', '#000');
+    root.style.setProperty('--theme-text-2', '#fff');
+    root.style.setProperty('--theme-text-3', '#909090');
+    root.style.setProperty('--theme-link', '#218cff');
+    root.style.setProperty('--theme-scroll-thumb', 'rgba(0,0,0,0.10)');
+    root.style.setProperty('--theme-scroll-track', 'rgba(0,0,0,0.10)');
 
     lightButton.style.display = "none";
     darkButton.style.display = "block";
@@ -57,8 +64,15 @@ function lightTheme() {
 function darkTheme() {
     root.style.setProperty('--theme-bg', '#202020');
     root.style.setProperty('--theme-bg-2', '#151515');
+    root.style.setProperty('--theme-bg-3', '#191919');
     root.style.setProperty('--theme-border', '#000');
+    root.style.setProperty('--theme-border-2', '#adadad');
     root.style.setProperty('--theme-text', '#fff');
+    root.style.setProperty('--theme-text-2', '#fff');
+    root.style.setProperty('--theme-text-3', '#909090');
+    root.style.setProperty('--theme-link', '#191919');
+    root.style.setProperty('--theme-scroll-thumb', 'rgba(255,255,255,0.10)');
+    root.style.setProperty('--theme-scroll-track', 'rgba(0,0,0,0.30)');
 
     darkButton.style.display = "none";
     lightButton.style.display = "block";
