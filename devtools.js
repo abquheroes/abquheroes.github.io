5ee26f487c72dd086599df43a132e8a255364ddf
akerson <pirodaggar@gmail.com>
2018-05-17 15:47:24 -0400

    devtools

--- devtools.js
+++ devtools.js
@@ -1,7 +1,16 @@
 const devtools = {
-    recipeCount : function() {
+    godmode : function() {
         for (let i=0;i<blueprints.length;i++) {
             itemCount[blueprints[i].name] = 1000;
         }
+        for (const [worker,lvl] of Object.entries(workerProgress)) {
+            workerProgress[worker] = 25;
+        }
+        for (const [upgrade,lvl] of Object.entries(upgradeProgress)) {
+            upgradeProgress[upgrade] = 5;
+        }
+        refreshRecipeSelector();
+        refreshWorkers();
+        refreshUpgrades();
     }
 }
--- tabs.js
+++ tabs.js
@@ -4,7 +4,7 @@ $(document).ready( () => {
     $("#tabs").tabs({
         activate: function( evt, ui ) {
             if ($(ui.newTab).index() === 0) {
-                //
+                refreshRecipeSelector();
             }
             else if($(ui.newTab).index() === 1) {
                 refreshWorkers();
