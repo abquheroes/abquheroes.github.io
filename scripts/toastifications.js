81ee41510923f05ae25664249bed2aba9831aaf3
zestyhamburger <richardpichardo@outlook.com>
2018-09-23 00:32:30 -0400

    random bullshit

--- scripts/toastifications.js
+++ scripts/toastifications.js
@@ -73,7 +73,7 @@ const Notifications = {
             position: 'top-right', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
             textAlign: 'left',  // Text alignment i.e. left, right or center
             loader: true,  // Whether to show loader or not. True by default
-            loaderBg: '#9EC600',  // Background color of the toast loader
+            loaderBg: '#FFF',  // Background color of the toast loader
         });
     },
     alreadyEquipped() {
@@ -88,7 +88,7 @@ const Notifications = {
             position: 'top-right', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
             textAlign: 'left',  // Text alignment i.e. left, right or center
             loader: true,  // Whether to show loader or not. True by default
-            loaderBg: '#9EC600',  // Background color of the toast loader
+            loaderBg: '#FFF',  // Background color of the toast loader
         });
     },
     moreBluePrints() {
@@ -103,7 +103,7 @@ const Notifications = {
             position: 'top-right', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
             textAlign: 'left',  // Text alignment i.e. left, right or center
             loader: true,  // Whether to show loader or not. True by default
-            loaderBg: '#9EC600',  // Background color of the toast loader
+            loaderBg: '#FFF',  // Background color of the toast loader
         });       
     }
 }
