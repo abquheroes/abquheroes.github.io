110692203f7b9dd06defc0f5f26f049938e0a847
zestyhamburger <richardpichardo@outlook.com>
2018-10-15 10:32:49 -0400

    adjustment: preloader

--- scripts/main.js
+++ scripts/main.js
@@ -43,7 +43,7 @@ function afterLoad() {
 
 const loading_screen = pleaseWait({
     logo: "images/site-logo.png",
-    backgroundColor: '#f46d3b',
+    backgroundColor: 'var(--bg)',
     loadingHtml: "<div class='sk-spinner sk-spinner-wave'><div class='sk-rect1'></div><div class='sk-rect2'></div><div class='sk-rect3'></div><div class='sk-rect4'></div><div class='sk-rect5'></div></div>"
 });
 loadMisc(); //the others are loaded in order
