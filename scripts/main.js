150d9fee66c555223b2a951161f5ecfa4854af90
zestyhamburger <richardpichardo@outlook.com>
2018-10-15 10:59:21 -0400

    adjustments: preloader

--- scripts/main.js
+++ scripts/main.js
@@ -38,13 +38,18 @@ function afterLoad() {
     populateRecipe("Knives");
     refreshProgress();
     setInterval(mainLoop, 10);
-    loading_screen.finish();
+    /*loading_screen.finish();*/
 }
+
+const loadMessage = "Placeholder text for random load messages.";
 
 const loading_screen = pleaseWait({
     logo: "images/site-logo.png",
     backgroundColor: 'var(--bg)',
-    loadingHtml: "<div class='sk-spinner sk-spinner-wave'><div class='sk-rect1'></div><div class='sk-rect2'></div><div class='sk-rect3'></div><div class='sk-rect4'></div><div class='sk-rect5'></div></div>"
+    loadingHtml: `
+    <div class="loadingMessage">${loadMessage}</div>
+    <div class="spinner"></div>
+    `
 });
 loadMisc(); //the others are loaded in order
 openTab("recipesTab");
